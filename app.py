74ac3ccb45fc655753f51b30fd97f7e245624678
Hosward Villoria <hosward.avc@gmail.com>
2023-12-01 17:34:17 -0300

    additional config, adds python flask as a service

--- app.py
+++ app.py
@@ -3,9 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from routes import users
 from models import db
 from flask_migrate import Migrate
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:pwd@localhost:15432/tasty_traverse_development"
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 app.config['SQLALCHEMY_RECORD_QUERIES'] = True
 db.init_app(app)
 Migrate(app, db)
@@ -14,4 +18,4 @@ app.register_blueprint(users.bp)
 
 if __name__ == '__main__':
     with app.app_context():
-        app.run(debug=True)
+        app.run(debug=True, host='0.0.0.0')
